b33f093628bb2b50d47c5d600d93fa6d1cd120ec
Tyler Nappy <tylernappy@gmail.com>
2014-10-30 22:46:54 -0400

    only people who haven't been sent a song in 24 hours show up on admin page

--- app/controllers/admin/send_emails_controller.rb
+++ app/controllers/admin/send_emails_controller.rb
@@ -2,11 +2,12 @@ class Admin::SendEmailsController < ApplicationController
   def index
    #   debugger
    #   debugger
-     @music_people = Search.new(MusicPerson, params[:music_person]).run
-   #   @music_people.order = 'updated_at'
+     @search_results = Search.new(MusicPerson, params[:music_person]).run #run search if any
+   #   debugger
+   #   debugger
+   #   @music_people = @search_results.select {|music_person| (music_person.first.updated_at - DateTime.current).abs > 24.hours} #only show people who haven't been sent an email
+     @music_people = @search_results.select {|music_person| (music_person.updated_at - DateTime.current).abs > 15.seconds} #only show people who haven't been sent an email
      @send_email = SendEmail.new
-
-   #   @search = Search.new(MusicPerson, params[:search])
       @search = MusicPerson.new
 
    #   debugger
@@ -14,15 +15,15 @@ class Admin::SendEmailsController < ApplicationController
   end
 
   def send_it
-     debugger
-     debugger
+   #   debugger
+   #   debugger
      ids = params[:send_email][:to].split("")
      ids.delete_at(0)
      ids.delete_at(ids.length-1)
      ids.delete_if {|number| number == "," || number == " " }
      ids.map! { |id| id.to_i }
-     debugger
-     debugger
+   #   debugger
+   #   debugger
      SendEmail.send_it(ids, params[:send_email][:song], params[:send_email][:artist], params[:send_email][:link], params[:send_email][:note])
    #   ids.each { |id| MusicPerson.find_by_id(id).update_attributes(last_song_sent: "#{params[:send_email][:song]} by #{params[:send_email][:artist]}") }
      redirect_to admin_send_emails_index_path, notice: 'Email sent'
